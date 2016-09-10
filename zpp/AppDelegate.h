433a55041a8e934803ad22545301b45995d2a202
DC20160426 <dc@zhangpengpeng.lan>
2016-09-10 14:19:06 +0800

    Initial Commit

---
+++ zpp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zpp
+//
+//  Created by DC20160426 on 16/9/10.
+//  Copyright © 2016年 DC20160426. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
